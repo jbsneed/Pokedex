8db12f7d9e70a40526a19bf40e57d18da72db64b
jbsneed <52945300+jbsneed@users.noreply.github.com>
2019-08-03 23:25:06 -0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var myName='Julie';
+document.write(myName);
+
+var simpleAddition = 2+2;
+document.write(simpleAddition);
+
+var size=100;
+var doubleSize= size * 2;
+document.write(doubleSize);
+
+var size=100;
+var doubleSize=size*2;
+var minSize= (doubleSize*2)-(size/2);
+document.write(minSize);
