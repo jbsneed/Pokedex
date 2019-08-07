e92123dd96a2ae493c5bda0431a1c5401d9ae23e
jbsneed <52945300+jbsneed@users.noreply.github.com>
2019-08-06 23:56:50 -0500

    Added pokemon to repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello World!');
-
-var favoriteFood='Fried Chicken';
-document.write(favoriteFood);
+var repository [
+  {name: 'Pikachu', height: 0.4, abilities:['static','lightningrod']},
+  {name: 'Charmander', height: 0.6, abilities:['blaze', 'solar-power']},
+  {name: 'Squirtle', height: 0.5, abilities:['rain-dish', 'torrent']},
+  {name: 'Bellsprout', height: 0.7, abilities:['chlorophyll', 'gluttony']},
+  {name: 'Seel', height: 1.1, abilities:['thick-fat','hydration','ice-body']}
+];
