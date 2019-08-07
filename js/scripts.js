97bab8ce37bea5478e559efb13448c5bbd9cf5b5
jbsneed <52945300+jbsneed@users.noreply.github.com>
2019-08-07 12:17:16 -0500

    Changed wording of repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var repository = [
-  {name: 'Pikachu', height: 0.4, abilities:['static','lightningrod']},
-  {name: 'Charmander', height: 0.6, abilities:['blaze', 'solar-power']},
-  {name: 'Squirtle', height: 0.5, abilities:['rain-dish', 'torrent']},
-  {name: 'Bellsprout', height: 0.7, abilities:['chlorophyll', 'gluttony']},
-  {name: 'Seel', height: 1.1, abilities:['thick-fat','hydration','ice-body']}
+  {name: 'Pikachu', height: 0.4, types:['static','lightningrod']},
+  {name: 'Charmander', height: 0.6, types:['blaze', 'solar-power']},
+  {name: 'Squirtle', height: 0.5, types:['rain-dish', 'torrent']},
+  {name: 'Bellsprout', height: 0.7, types:['chlorophyll', 'gluttony']},
+  {name: 'Seel', height: 1.1, types:['thick-fat','hydration','ice-body']}
 ];
